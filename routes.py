27ee52b58c8e04f962400417a8954386cf5d16b1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-19 13:47:40 +1200

    Feedback page works and displays information into a hidden admin page (security through obscurity). Need to fix how the information is displayed on admin page.

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import sqlite3
 app = Flask(__name__)
 
@@ -24,31 +24,43 @@ def goaltender(id):
     conn = sqlite3.connect("goalies.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM Statistics JOIN Goaltender ON Statistics.Goaltender_ID = Goaltender.ID WHERE ID=?;",(id,))
-    # cur.execute("SELECT * FROM Goaltender WHERE Goaltender_ID=?",(id,))
     goaltender = cur.fetchone()
     return render_template('goaltender.html', goaltender=goaltender)
 
-
- 
-#  WHERE Goaltender_ID=?",(id,))
-    
-
+#Feedback function...
 
 #Feedback page
 @app.route('/feedback')
 def feedback():
     return render_template('feedback.html')
 
+#Finished feedback page
+@app.route('/submitedfeedback')
+def submitedfeedback():
+    return render_template('submitedfeedback.html')
+
+#Submits feedback into goalie database and directs user to a 'return to home page'
+@app.route('/submitedfeedback', methods=['POST'])
 def submit_feedback():
     name = request.form['name']
     message = request.form['message']
+    topic = request.form['topic']
     conn  = sqlite3.connect('goalies.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Feedback FROM (name, message, topic) VALUES (?,?,?)', (name, message))
+    cur = conn.cursor()
+    cur.execute('INSERT INTO Feedback (name, message, topic) VALUES (?,?,?)', (name, message, topic))
     conn.commit()
     conn.close()
+    return redirect('/submitedfeedback')
 
-    return 'Feedback sent!'
+#Hidden admin page for feedback display
+@app.route('/admin')
+def admin():
+    conn = sqlite3.connect("goalies.db")
+    cur = conn.cursor()
+    cur.execute("SELECT name, message, topic FROM Feedback")
+    feedback = cur.fetchall()
+    conn.close()
+    return render_template("admin.html", feedback=feedback)
 
 if __name__ == "__main__":
     app.run(debug=True)
